f59bfb48992f584f4ef1c88f6f769b29f19f2d5d
Burinthorn T. <n4secure.t@gmail.com>
2025-12-30 21:22:40 +0700

    Fix white screen components and build config

--- OceanGrass/vite.config.js
+++ OceanGrass/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/OceanGrass/', 
+  base: '/OceanGrass/',
+  server: {
+    open: '/OceanGrass/',
+  },
 })
