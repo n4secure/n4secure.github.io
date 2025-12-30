0200f04706168006c202de9eb1b58891a857af2f
Burinthorn T. <n4secure.t@gmail.com>
2025-12-30 21:37:35 +0700

    Refactor entry point to allow auto-redirect from project root to dist

--- OceanGrass/vite.config.js
+++ OceanGrass/vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/OceanGrass/dist/',
   server: {
-    open: '/OceanGrass/dist/',
+    open: '/source.html',
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'source.html'),
+      },
+    },
   },
 })
